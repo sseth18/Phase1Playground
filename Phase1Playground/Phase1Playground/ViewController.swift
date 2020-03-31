3441c23eb4c1940851de97852950d2f7fa22ba96
sseth18 <samar.seth@gmail.com>
2020-03-30 23:42:06 -0400

    Initial App commit

---
+++ Phase1Playground/Phase1Playground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Phase1Playground
+//
+//  Created by Samar Seth on 3/30/20.
+//  Copyright © 2020 Samar Seth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
